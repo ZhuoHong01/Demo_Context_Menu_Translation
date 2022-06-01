5a06f8a43ee41852f5de41b996b698852e91c6ca
lee zhuo hong <21018193@myrp.edu.sg>
2022-06-01 09:52:48 +0800

    e

--- app/src/main/java/sg/edu/rp/c346/id21018193/democontextmenutranslation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/democontextmenutranslation/MainActivity.java
@@ -3,12 +3,18 @@ package sg.edu.rp.c346.id21018193.democontextmenutranslation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+        registerForContextMenu(tvTranslatedText);
     }
 }
