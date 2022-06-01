add9df720bf7c9fc63624d6636de5537e869da49
lee zhuo hong <21018193@myrp.edu.sg>
2022-06-01 10:02:41 +0800

    e

--- app/src/main/java/sg/edu/rp/c346/id21018193/democontextmenutranslation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/democontextmenutranslation/MainActivity.java
@@ -3,6 +3,8 @@ package sg.edu.rp.c346.id21018193.democontextmenutranslation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
 
         tvTranslatedText = findViewById(R.id.textViewTranslatedText);
         registerForContextMenu(tvTranslatedText);
+    }
+
+//    @Override
+//    public void OnCreateContextMenu(ContextMenu menu, View v,
+//                                    ContextMenu.ContextMenuInfo menuInfo){
+//        super.onCreateContextMenu(menu, v, menuInfo);
+//
+//    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0,0,0,"English");
+        menu.add(0,1,1,"Italian");
     }
 }
